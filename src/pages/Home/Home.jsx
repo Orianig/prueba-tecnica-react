3869a46e74a8d93d8eb0be3373a478978c70f368
orianig <oriana.ig016@gmail.com>
2023-06-29 13:13:35 +0200

    feat: add nested fetch - style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Home.css";
 
-export const Home = () => {
+function Home() {
+  //establezco los hoks
+  //guarda la data
+  const [result, setResult] = useState([]);
+  //guarda la lista de pokemones
+  const [poke, setPoke] = useState([]);
+
+  const [load, setLoad] = useState("true");
+
+  const pokemon = [];
+
+  useEffect(() => {
+    //utilizo un fetch para la llamada
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=12") // lamada a la API, el resultado es una promesa
+      .then((response) => response.json()) //transformamos la respuesta a JSON (response.json() también como promesa
+      .then((data) => //respuesta en formato objeto
+        setResult(
+          data.results.map((item) => {
+            fetch(item.url)
+              .then((response) => response.json())
+              .then((allpokemon) => pokemon.push(allpokemon));
+            setPoke(pokemon);
+          })
+        )
+      );
+  }, []);
+
+  setTimeout(() => {
+    setLoad(false);
+    console.log(poke);
+  }, 1000);
+
   return (
-    <div>
-      <h1>SOY HOME</h1>
+    <div className="Home">
+      <div className="pokegallery">
+        {load ? (
+          <p>Loading...</p>
+        ) : (
+          poke.map((img, i) => (
+            <div id={img.id} key={img.id}>
+              <div className="card">
+                <img src={img.sprites.front_default} alt="pokemon" />
+                <div>
+                  <h2>{img.name}</h2>
+                  <h4>type: {img.types[0].type.name}</h4>
+                </div>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
-};
+}
 
 export default Home;
---
+++ src/services/apiCalls.js
@@ -0,0 +1,13 @@
+// import axios from 'axios';
+
+// const root = "https://pokeapi.co/api/v2/pokemon/"
+
+// export const bringPokemon = async () => {
+
+//     return await axios.get(`https://pokeapi.co/api/v2/pokemon/`);
+// };
+
+// export const searchCharacter = async (criteria) => {
+
+//     return await axios.get(`https://pokeapi.co/api/v2/pokemon/${criteria}`);
+// }
